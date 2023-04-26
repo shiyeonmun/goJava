daad79e80079bcdd70536f53e11e934bbab63f7d
shiyeonmun <mun1348@gmail.com>
2023-04-26 18:04:49 +0900

    abstract / interface

---
+++ ch07/sec08/CarExample.java
@@ -0,0 +1,21 @@
+package ch07.sec08;
+
+public class CarExample {
+	
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		
+		myCar.tire = new Tire();
+		myCar.run();
+		
+		myCar.tire = new HankookTire();
+		myCar.run();
+
+		myCar.tire = new KumhoTire();
+		myCar.run();
+		
+		
+
+	}
+
+}
---
+++ ch07/sec08/exam01/Driver.java
@@ -0,0 +1,8 @@
+package ch07.sec08.exam01;
+
+public class Driver{
+
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
